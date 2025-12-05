a3e639a1a9a1963fdd68fc81e07d7c59bd7e22d1
manishroy <manishroy0112@gmail.com>
2025-12-05 14:28:37 +0000

    tcp server client

---
+++ tcp-server-client/client.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// struct in_addr {
+//     uint32_t s_addr; // IPv4 in big-endian format
+// };
+
+// struct stockaddr_in {
+//     uint16_t sin_family; // Address family (e.g., AF_INET)
+//     uint16_t sin_port;  // Port number in big-endian format
+//     struct in_addr sin_addr;  // IP address (e.g., IPv4)
+// };
+
+static void die(const char *msg) {
+    int error = errno;
+    fprintf(stderr, "[%d] %s\n", error, msg);
+    abort();
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd) {
+        die("socket()");
+    }
+
+    struct sockaddr_in srv_addr = {};
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = ntohs(1234);
+    srv_addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
+
+    int rv = connect(fd, (const sockaddr *)&srv_addr, sizeof(srv_addr));
+    if (rv) {
+        die("connect");
+    }
+
+    char msg[] = "Hello from client";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf)-1);
+    if (n < 0) {
+        die("read");
+    }
+    printf("Received from server: %s\n", rbuf);
+    close(fd);
+
+    return 0;
+}
---
+++ tcp-server-client/server.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// struct in_addr {
+//     uint32_t s_addr; // IPv4 in big-endian format
+// };
+
+// struct sockaddr_in {
+//     uint16_t sin_family; // Address family (e.g., AF_INET)
+//     uint16_t sin_port;  // Port number in big-endian format
+//     struct in_addr sin_addr;  // IP address (e.g., IPv4)
+// };
+
+static void die(const char *msg) {
+    int error = errno;
+    fprintf(stderr, "[%d] %s\n", error, msg);
+    abort();
+}
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void do_something(int connfd){
+    // one read
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf)-1);
+    if(n < 0){
+        msg("read() error");
+        return;
+    }
+
+    // one write (no error handling for brevity)
+    char wbuf[] = "Hello from server";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
+int main() {
+    /* s1: obtain socket handler
+    AF_INET: IPv4 Internet protocols
+    SOCK_STREAM: TCP*/
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // s2: set socket options
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // s3: bind socket to address and port
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234); // port
+    addr.sin_addr.s_addr = htonl(0); // IP 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv > 0) { die("bind()"); }
+
+    /* s4: listen for incoming connections
+    socket is actually created here */
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    // s5: accept a connection & close it
+    while(true){
+        struct sockaddr_in client_addr = {};
+        socklen_t client_len = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &client_len);
+        if (connfd < 0) {
+            continue; // ERROR
+        }
+
+        // s6: read/write data
+        do_something(connfd);
+
+        // Close connection
+        close(connfd);
+    }
+
+    return 0;
+}
